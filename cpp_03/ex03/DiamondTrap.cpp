44e45914603d14caf6a5451f6ef2ea9b26b3055c
bkwag <kwag93@naver.com>
2021-07-24 09:50:49 +0900

    flag 추가

--- cpp_03/ex03/DiamondTrap.cpp
+++ cpp_03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: bkwag <bkwag@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/16 15:20:32 by bkwag             #+#    #+#             */
-/*   Updated: 2021/07/19 15:09:45 by bkwag            ###   ########.fr       */
+/*   Updated: 2021/07/23 13:57:32 by bkwag            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,20 @@
 
 DiamondTrap::DiamondTrap()
 {
-	std::cout<<"Default constructor DiamondTrap"<<std::endl;
+	std::cout << "Default constructor DiamondTrap" << std::endl;
 }
 
-DiamondTrap::DiamondTrap(std::string name):ClapTrap(name), FragTrap(name), ScavTrap(name)
+DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name), FragTrap(name), ScavTrap(name)
 {
-	std::cout<<"Constructor DiamondTrap"<<std::endl;
+	std::cout << "Constructor DiamondTrap" << std::endl;
 	this->name = name;
 	this->HitPoint = this->FragTrap::HitPoint;
 	this->EnergyPoint = this->ScavTrap::EnergyPoint;
 	this->AttackDamage = this->FragTrap::AttackDamage;
-	this->ClapTrap::name = name+"_clap_name";
+	this->ClapTrap::name = name + "_clap_name";
 }
 
-DiamondTrap::DiamondTrap(const DiamondTrap& copy):ClapTrap(copy.name)
+DiamondTrap::DiamondTrap(const DiamondTrap &copy) : ClapTrap(copy.name)
 {
 	this->name = copy.name;
 	this->HitPoint = copy.HitPoint;
@@ -37,10 +37,10 @@ DiamondTrap::DiamondTrap(const DiamondTrap& copy):ClapTrap(copy.name)
 
 DiamondTrap::~DiamondTrap()
 {
-	std::cout<<"Destructor DiamondTrap"<<std::endl;
+	std::cout << "Destructor DiamondTrap" << std::endl;
 }
 
-DiamondTrap& DiamondTrap::operator=(const DiamondTrap& copy)
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &copy)
 {
 	this->name = copy.name;
 	this->HitPoint = copy.HitPoint;
